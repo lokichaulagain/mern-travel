90d96ed118c1a5e25faf8343d7157d5c6b429270
lokendra <lokendrachaulagain803@gmail.com>
2022-08-24 23:16:18 +0545

    good night

---
+++ frontend/src/pages/events.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EventCard from "../components/EventCard";
+
+function events() {
+  return (
+    <div>
+      <EventCard />
+    </div>
+  );
+}
+
+export default events;
