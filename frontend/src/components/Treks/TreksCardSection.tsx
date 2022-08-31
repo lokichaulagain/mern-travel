58aeac1eb5f4976ea527604a37ba5558a159e1da
lokendra <lokendrachaulagain803@gmail.com>
2022-08-31 08:27:50 +0545

    trek page completed

---
+++ frontend/src/components/Treks/TreksCardSection.tsx
@@ -0,0 +1,205 @@
+import TrekCard from "./TrekCard";
+
+const TreksCardSection = () => {
+  return (
+    <div className="py-5 " style={{ backgroundColor: "#EFF3F6" }}>
+      <div className="container px-5">
+        <div className="container m-5 mt-0">
+          {/* navTab */}
+          <ul
+            className="nav nav-pills mx-2 mb-3 py-3 fw-bolder gap-3 "
+            id="pills-tab"
+            role="tablist"
+          >
+            <li className="nav-item" role="presentation">
+              <button
+                className="btn categoryButton active"
+                id="pills-all-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-all"
+                type="button"
+                role="tab"
+                aria-controls="pills-all"
+                aria-selected="true"
+              >
+                ALL
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="btn categoryButton"
+                id="pills-hiking-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-hiking"
+                type="button"
+                role="tab"
+                aria-controls="pills-hiking"
+                aria-selected="false"
+              >
+                HIKING{" "}
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="btn categoryButton"
+                id="pills-treking-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-treking"
+                type="button"
+                role="tab"
+                aria-controls="pills-treking"
+                aria-selected="false"
+              >
+                TREKING
+              </button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button
+                className="btn categoryButton"
+                id="pills-canyoning-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-canyoning"
+                type="button"
+                role="tab"
+                aria-controls="pills-canyoning"
+                aria-selected="false"
+              >
+                CANYONING
+              </button>
+            </li>
+
+            <li className="nav-item" role="presentation">
+              <button
+                className="btn categoryButton"
+                id="pills-adventure-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-adventure"
+                type="button"
+                role="tab"
+                aria-controls="pills-adventure"
+                aria-selected="false"
+              >
+                ADVENTURE
+              </button>
+            </li>
+          </ul>
+
+          <div className="tab-content mx-2" id="pills-tabContent">
+            <div
+              className="tab-pane fade  show active "
+              id="pills-all"
+              role="tabpanel"
+              aria-labelledby="pills-all-tab"
+            >
+              <div className="row">
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+              </div>
+            </div>
+            <div
+              className="tab-pane fade"
+              id="pills-hiking"
+              role="tabpanel"
+              aria-labelledby="pills-hiking-tab"
+            >
+              <div className="row">
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tab-pane fade"
+              id="pills-treking"
+              role="tabpanel"
+              aria-labelledby="pills-treking-tab"
+            >
+              <div className="row">
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tab-pane fade"
+              id="pills-canyoning"
+              role="tabpanel"
+              aria-labelledby="pills-canyoning-tab"
+            >
+              <div className="row">
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tab-pane fade"
+              id="pills-adventure"
+              role="tabpanel"
+              aria-labelledby="pills-adventure-tab"
+            >
+              <div className="row">
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+
+                <div className="col-4">
+                  <TrekCard />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TreksCardSection;
--- frontend/src/pages/trek/index.tsx
+++ frontend/src/pages/trek/index.tsx
@@ -3,17 +3,17 @@ import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import TreksContactusCard from "../../components/Treks/TreksContactusCard";
 import TreksServiceCard from "../../components/Treks/TreksServiceCard";
-import TreksCard from "../../components/Treks/TreksCard";
 import styles from "../../styles/scss/Treks.module.scss";
 import HeroComponent from "../../components/HeroComponent";
-import TreksCategoryNavbar from "../../components/Treks/TreksCategoryNavbar";
+
+import TreksCardSection from "../../components/Treks/TreksCardSection";
 
 function Trek() {
   return (
     <div className={`${styles.pageWrapper}`}>
       <Navbar />
       <HeroComponent />
-      <TreksCard />
+      <TreksCardSection />
       <TreksContactusCard />
       <TreksServiceCard />
       <Footer />
